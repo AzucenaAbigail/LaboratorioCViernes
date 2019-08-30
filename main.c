e79905faf96c3f8fa2f0d4c35836a5a03bc8b291
azucena abigail <abigailmorin01@gmail.com>
2019-08-30 09:37:58 -0500

    Practica4

--- main.c
+++ main.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+/* Actividad #1,introdusca dos numeros por teclado e imprima en pantalla el numero más grande.
+Azucena Abigail Morin Inungaray   1872953 */
 
-int main(int argc, char *argv[]) {
+int main()
+{
+int numero1,numero2;
+ printf("introducir un numero1\n");
+ printf("introducir un numero2\n");
+  scanf("%d,&nuemro1\n");
+  scanf("%d,&numero2\n");
+if(numero1>=numero2)
+ printf("es el mayor");
+
+
+/*Actividad#2,Crea un programa que declare tres variablesde tipo entero A=5,B=3,C=-12 y muestre si se cumple
+o no las sigientes condiciones,utilizando el if, else.*/
+	int a=5,b=3,c=-12;
+	printf("a=5,b=3,c=-12");
+	printf("a>3\n");
+	
+	
+	
+	
 	return 0;
+	
+	
+	
 }
+
+
