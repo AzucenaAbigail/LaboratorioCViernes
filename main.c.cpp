cd0f59b55d8a9490d9ba90b62de1ab975d90266d
Azucena Abigail <abigailmorin01@gmail.com>
2019-09-06 09:09:43 -0500

    actividad 1

---
+++ main.c.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/*Actividad#1 Viernes,06,septiembre,2019    Azucena Abigail Morin Inungaray
+   1872953  LaboratorioC-viernes*/
+   
+   int main()
+{
+   	int mat,nota;
+   	 printf("Nota de materia:");
+   	  scanf("%d",&mat);
+   	nota>=5
+   	printf("Aprobado");
+   	
+   	nota>=0
+   	 printf("No Aprobado");
+   	
+   	nota<5
+   	 printf("Error");
+   	 
+	 return 0;
+}
