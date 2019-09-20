7f92e34413783f9d39d8fa30f289b9812e35b23c
AzucenaAbigail2 <abigailmorin01@gmail.comgit config --global user.name AzucenaAbigail2>
2019-09-20 09:09:53 -0500

    practica7

--- main.c
+++ main.c
@@ -1,8 +1,26 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+/*Practica#7  Actividad 1;Pida por teclado 10 calificaiones de un alunmo correspondiente a 
+   10 examenes...*/
 
-int main(int argc, char *argv[]) {
-	return 0;
+int main() {
+	int cal1,cal2,cal3,cal4,cal5,cal6,cal7,cal8,cal9,cal10;
+	int exam,prom;
+	 
+	 printf("  Practica #7    Actividad#1 ");
+	 
+	 printf("\n Digite 5 calificaion de examen: ");
+	  scanf("%d %d %d %d %d",&cal1,&cal2,&cal3,&cal4,cal5);
+	 
+	 printf("\n Digite 5 calificaiones de examen: ");
+	  scanf("%d %d %d %d %d",&cal6&cal7,&cal8,&cal9,&cal10);
+    
+    prom=(cal1+cal2+cal3+cal4+cal5)/5;
+
+
+/*Actividad 2; Escibe un programa que utilice el bucle for para calcular la poencia
+     de un numero...*/
+     int base,exp;
+     
+return 0;
 }
