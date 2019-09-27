3e4a943c0e4657cdfa7f01c7b91fb1010fa064cc
Aula211-13 <Azucena Abigail>
2019-09-27 09:01:41 -0500

    Practica8

--- main.c
+++ main.c
@@ -1,8 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
+int main() {
+	printf(" Actividad 1\n12");
+	printf("Numero de Coches;");
+	
+	int i,R;
+	int NdC[24];
+	 for(i=0;i<=24;i++){
+	 	scanf("%d",&NdC);
+	 }
 	return 0;
 }
