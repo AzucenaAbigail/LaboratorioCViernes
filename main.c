14eb2e342bbccbb97d021572ef0c86da2edd8027
HeberSegura <heber.segura.gonzalez@gmail.com>
2019-10-11 08:56:34 -0500

    Practica10

--- main.c
+++ main.c
@@ -1,8 +1,42 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+#include <string.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+/* Practica 10
+    Actividad #1 
+	Azucena Abigail Morin Inungaray 1872953
+	11/10/19  Viernes 7 am -9 am*/
 
-int main(int argc, char *argv[]) {
+int main() {
+	
+	printf("Practica 10  \n");
+	printf("Actividad #1\n");
+	printf("\n");
+
+	char cad[30];
+	int i;
+     printf("Introdusca una palabra: ");
+     scanf("%s",cad);
+    int x=strlen(cad);
+    for(i=x-1;i>=0;i--){
+    	printf("%c",cad[i]);
+	}
+	
+	
+	
+/*	printf("\n\n");
+	
+	printf("Actividad #2\n");
+	
+	char cad[30];
+	int i,s;
+	 printf("Introdusca varias letras: ");
+	  scanf("%s",cad);
+	
+	int strcat(cad);
+	for(i=x+1;i>=0;i++){
+	   s=cad+cad;
+		printf("%c",cad[i]);
+	}*/
+	
 	return 0;
 }
